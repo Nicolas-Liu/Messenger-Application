ad4497d3bb112de6a04d08f6ca20274ef5ab95dc
AzureAD\NicolasLiu <nliu@idside.com>
2022-11-11 20:43:49 -0500

    css

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import Alert from "@mui/material/Alert";
 
 const Register = () => {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <div className="formContainer">
+      <Snackbar open={open} autoHideDuration={4500} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Success. Thank you!
+        </Alert>
+      </Snackbar>
+
       <div className="formWrapper">
         <span className="title">Connect with your loved ones</span>
         <span>Register</span>
@@ -32,12 +57,14 @@ const Register = () => {
             />
             <span>Add an avatar</span>
           </label>
-          <Button variant="outlined">Sign up</Button>
+          <Button variant="outlined" onClick={handleClick}>
+            Sign up
+          </Button>
         </form>
         <p>
           Got an account?
           <Link to="/login" className="linkRouter">
-            <span>Login</span>
+            <span> Login</span>
           </Link>
         </p>
       </div>
