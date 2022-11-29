fb42e5eaa7c6c53790911e2c65b64a93d817a14f
unknown <nicolas.liu1@hotmail.com>
2022-11-28 19:47:00 -0500

    refacotring

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,23 +6,16 @@ import { getFirestore } from "firebase/firestore";
 
 console.log('process.env: ', process.env)
 
+
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "chat-f65ed.firebaseapp.com",
-  projectId: "chat-f65ed",
-  storageBucket: "chat-f65ed.appspot.com",
-  messagingSenderId: "790434673092",
-  appId: "1:790434673092:web:44cb32fdda02a6f5e4b491"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
 };
-
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-//   appId: process.env.REACT_APP_FIREBASE_APPID
-// };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
